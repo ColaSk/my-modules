4a3a37992993bff8ed2f39b20d66f6f033cbd7fa
kx <kaixuan.sun@boonray.com>
2022-02-25 16:04:36 +0800

    添加redis 分布式锁实现

--- myredis/myredis/client.py
+++ myredis/myredis/client.py
@@ -1,1 +1,25 @@
 import redis
+import datetime
+from typing import Any, Union
+
+pool = redis.ConnectionPool()
+
+class RedisClient(object):
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.client = redis.Redis(*args, **kwargs)
+
+    def __getattr__(self, __name: str) -> Any:
+        return getattr(self.client, __name)
+
+class RedisBase:
+
+    _prefix_key = "prefix"
+    _db = 0
+
+    def __init__(self, client:  Union[RedisClient, redis.Redis]):
+        self._client = client
+
+    @property
+    def client(self):
+        return self._client
---
+++ myredis/myredis/dcs_lock.py
@@ -0,0 +1,68 @@
+import datetime
+from .client import RedisBase
+
+class DistributedLock(RedisBase):
+
+    """双重分布式锁"""
+
+    _key = "dcs_lock"
+    _timeout = 3600 # s
+
+    @property
+    def key(self):
+        return f'{self._prefix_key}:{self._key}'
+    
+    def get(self, key):
+        velue = self.client.get(key)
+        return velue
+
+    def setnx(self, key, velue):
+        return self.client.setnx(key, velue)
+    
+    def getset(self, key, velue):
+        old_velue = self.client.getset(key, velue)
+        return old_velue
+    
+    def expire(self, key, seconds):
+        return self.client.expire(key, seconds)
+    
+    def delete(self, *key):
+        return self.client.delete(*key)
+
+    def create_lock(self):
+        """创建锁
+        key:value
+        key:时间戳+超时时间
+        """
+        curr_time = datetime.now().timestamp()
+        end_time = curr_time + self._timeout
+
+        # 获取锁
+        lock = self.setnx(self.key, end_time)
+
+        # 获取到锁, 设置超时时间
+        if lock:
+            self.expire(self.key, self._timeout)
+            return str(end_time)
+
+        # 获取旧锁
+        oldlock = self.get(self.key)
+
+        # 当前申请锁的时间大于旧的锁
+        if oldlock and curr_time > float(oldlock):
+
+            _oldlock = self.getset(self.key, end_time)
+            if not _oldlock or oldlock == _oldlock:
+                return str(end_time)
+
+            return
+
+        return
+
+    def delete_lock(self):
+        """删除锁"""
+        return self.delete(self.key)
+
+    def verify_lock(self, verify):
+        """验证锁"""
+        return bool(self.get(self.key) == verify)
